212ce1727e99214f9debc50b03fa5c8736912679
github username <kelly.hesterly@gmail.com>
2022-01-12 22:58:45 -0700

    commit tree 0ce169dd607fc6d811890aaf77843d5dd99b2b11

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,4 +1,8 @@
 class StudentsController < ApplicationController
+    def index
+        @students = Student.all
+    end
+
     def show
         @student = Student.find(params[:id])
     end
@@ -7,29 +11,25 @@ class StudentsController < ApplicationController
         @student = Student.new
     end
 
-    def index
-        @students = Student.all
+    def create
+        @student = Student.new(student_params)
+        @student.save
+        redirect_to student_path(@student)
     end
 
     def edit
         @student = Student.find(params[:id])
     end
 
-    def create
-        @student = Student.new(student_params(:first_name, :last_name))
-        @student.save
-        redirect_to student_path(@student)
-    end
-
     def update
         @student = Student.find(params[:id])
-        @student.update(student_params(:first_name, :last_name))
+        @student.update(student_params)
         redirect_to student_path(@student)
     end
 
     private
-    
-    def student_params(*args)
-        params.require(:student).permit(*args)
+
+    def student_params
+        params.require(:student).permit(:first_name, :last_name)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :students, only: [:index, :update, :show, :new, :create, :edit]
-  resources :school_classes, only: [:index, :update, :show, :new, :create, :edit]
+resources :school_classes
+resources :students
+
 end
